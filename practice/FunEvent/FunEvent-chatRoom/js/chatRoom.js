69b5477398e7a99a1b5ce65adeb646c0d80c07e5
Dustin085 <dustin830927@gmail.com>
2024-07-24 18:34:29 +0800

    0724

--- practice/FunEvent/FunEvent-chatRoom/js/chatRoom.js
+++ practice/FunEvent/FunEvent-chatRoom/js/chatRoom.js
@@ -4,34 +4,53 @@ console.log('Hi ! you linked chatRoom.js');
 // 2. 做一個假訊息在html以確定樣式(約20mins) OK
 // 3. 確認btn和textarea的用法 OK
 // 4. 用js塞chatroom.msg到html裡面達到創建新訊息的效果 OK 使用了innerHTML，不易閱讀，之後再找看看有沒有別的方法
-// 5. 寫一個利用按鈕與textarea來創建新訊息的函式
-// 6. 把兩人的訊息分邊
-// 7. 開始製作ChatRoom物件樣版(class)
-// 製作ChatRoom物件樣版
+// 5. 寫一個利用按鈕與textarea來創建新訊息的函式 OK 直接寫好了eventlistener
+// 6. 把兩人的訊息分邊 OK 分成fromUser跟fromFunEvent
+// 7. 開始製作ChatRoom物件樣版(class) NO 不需要
+// TODO list
+// 1. 加上一個送出新訊息之後自動卷軸到最下方
+// 2. 修正文字訊息過長不會自動換行的問題
 
 // let crBtnEle = document.getElementById('chatroom__btn');
 let crBtnEle = document.querySelector('#chatroom__btn');
 let crInputEle = document.querySelector('#chatroom__input');
 let crBodyEle = document.querySelector('#chatroom__body')
 
-// 按下按鈕後
+// 按下按鈕後，把使用者輸入的文字包裝成html元素放到聊天室內
 function creatNewMsg(btnEle, inputEle, crBodyEle) {
-    btnEle.addEventListener("click", ()=>{
-        crBodyEle.innerHTML += `<div class="chatroom__msg" id="chatroom__msg01"><div class="avatar"></div><div class="content--container"><div class="name"><p>FunEvent 小幫手</p></div><div class="text"><p>${inputEle.value}</p></div></div></div>`;
-        inputEle.value='';
+    btnEle.addEventListener("click", () => {
+        // crBodyEle.innerHTML += `<div class="chatroom__msg" id="chatroom__msg01"><div class="avatar"></div><div class="content--container"><div class="name"><p>FunEvent 小幫手</p></div><div class="text"><p>${inputEle.value}</p></div></div></div>`;
+        // 輸入的文字
+        let inputText = inputEle.value;
+        // 使用者頭像與名稱
+        let userAvatarImgUrl = './images/cola.png';
+        let userName = '使用者名字';
+        if (inputText != '') {
+            // 加入新元素，class的css已經設定好了
+            crBodyEle.innerHTML +=
+                `<div class="chatroom__msg fromUser">
+                <div class="avatar" style="background-image: url(${userAvatarImgUrl});background-size: cover;"></div>
+                <div class="content--container">
+                <div class="name"><p>${userName}</p></div>
+                <div class="text"><p>${inputEle.value}</p></div>
+                </div>
+                </div>`;
+            inputEle.value = '';
+        }
     });
-    inputEle.addEventListener("keypress", (event)=>{
-        if(event.key === "Enter"){
+    // 在input區域按下enter直接觸發btn點擊事件
+    inputEle.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
             event.preventDefault();
-            // 幫我點btnEle
+            // 觸發點擊事件
             btnEle.click();
         }
     });
 }
 creatNewMsg(crBtnEle, crInputEle, crBodyEle);
 
-// 直接塞到crBodyEle
-crBodyEle.innerHTML += '<div class="chatroom__msg" id="chatroom__msg01"><div class="avatar"></div><div class="content--container"><div class="name"><p>FunEvent 小幫手</p></div><div class="text"><p>測試訊息</p></div></div></div>';
+
+
 
 // 塞測試用msg進crBodyEle
 // myDiv = document.createElement('div');
@@ -48,9 +67,9 @@ crBodyEle.innerHTML += '<div class="chatroom__msg" id="chatroom__msg01"><div cla
 // test
 
 
-function ChatRoom(convsationBody, inputEle, submitBtnEle) {
-    this.convsationBody = convsationBody;
-    this.inputEle = inputEle;
-    this.submitBtnEle = submitBtnEle;
-}
+// function ChatRoom(convsationBody, inputEle, submitBtnEle) {
+//     this.convsationBody = convsationBody;
+//     this.inputEle = inputEle;
+//     this.submitBtnEle = submitBtnEle;
+// }
 
