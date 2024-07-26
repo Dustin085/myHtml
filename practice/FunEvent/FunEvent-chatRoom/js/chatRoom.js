1989c9bc1a8e68e74d22a131c7220db52ad1b419
Dustin085 <dustin830927@gmail.com>
2024-07-26 20:17:13 +0800

    edit-chatroomTODOist

--- practice/FunEvent/FunEvent-chatRoom/js/chatRoom.js
+++ practice/FunEvent/FunEvent-chatRoom/js/chatRoom.js
@@ -10,6 +10,10 @@ console.log('Hi ! you linked chatRoom.js');
 // TODO list
 // 1. 加上一個送出新訊息之後自動卷軸到最下方 OK function scrollBack
 // 2. 修正文字訊息過長不會自動換行的問題 OK word-break: break-all; 要設定最大寬度，然後好幾個元素都要加上width: 100%讓文字訊息有足夠的寬度
+// 3. 把訊息做成物件樣版(class)
+// 4. 把訊息物件存在local storage
+// 5. 寫一個initChatRoom，來初始化聊天室
+// 6. 創建多個聊天室，可能把聊天室寫成class來製造多個聊天室
 
 // let crBtnEle = document.getElementById('chatroom__btn');
 let crBtnEle = document.querySelector('#chatroom__btn');
@@ -27,14 +31,8 @@ function addChatRoomListener(btnEle, inputEle, crBodyEle) {
         let userName = '使用者名字';
         if (inputText != '') {
             // 加入新元素，class的css已經設定好了
-            crBodyEle.innerHTML +=
-                `<div class="chatroom__msg fromUser">
-                <div class="avatar" style="background-image: url(${userAvatarImgUrl});background-size: cover;"></div>
-                <div class="content--container">
-                <div class="name"><p>${userName}</p></div>
-                <div class="text"><p>${inputEle.value}</p></div>
-                </div>
-                </div>`;
+            crBodyEle.innerHTML += creatChatRoomMsgHtmlStr(userAvatarImgUrl, userName, inputEle.value);
+            // 洗掉輸入框內的文字
             inputEle.value = '';
             // 自動卷軸到最下面
             scrollBack(crBodyEle);
@@ -53,10 +51,36 @@ function addChatRoomListener(btnEle, inputEle, crBodyEle) {
         scrollEle.scrollTo(0, scrollEle.scrollHeight - scrollEle.clientHeight);
     }
 }
+// 製造聊天室訊息的html string，參數分別為：頭像圖片的url字串、名稱、文字、是否來自使用者(正在看網頁的那個會員)
+function creatChatRoomMsgHtmlStr(avatarImgUrl, name, text, isFromUser = true) {
+    // 回傳html string，樣式都已經在css內做好了。頭像圖片直接寫在html屬性裡
+    let fromWho = '';
+    if(isFromUser){
+        fromWho = "fromUser";
+    }else{
+        fromWho = "fromOther";
+    }
+    return `<div class="chatroom__msg ${fromWho}">
+            <div class="avatar" style="background-image: url(${avatarImgUrl});"></div>
+            <div class="content--container">
+            <div class="name"><p>${name}</p></div>
+            <div class="text"><p>${text}</p></div>
+            </div>
+            </div>`;
+}
+// 執行func
 addChatRoomListener(crBtnEle, crInputEle, crBodyEle);
 
 
 
+
+// `<div class="chatroom__msg fromUser">
+// <div class="avatar" style="background-image: url(${userAvatarImgUrl});background-size: cover;"></div>
+// <div class="content--container">
+// <div class="name"><p>${userName}</p></div>
+// <div class="text"><p>${inputEle.value}</p></div>
+// </div>
+// </div>`;
 
 // 塞測試用msg進crBodyEle
 // myDiv = document.createElement('div');
