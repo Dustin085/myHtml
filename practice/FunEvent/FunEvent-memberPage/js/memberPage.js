d9a370cb402a0a9b8ab017e4dd632c7e9f0670ee
Dustin085 <dustin830927@gmail.com>
2024-07-30 02:52:07 +0800

    0730

--- practice/FunEvent/FunEvent-memberPage/js/memberPage.js
+++ practice/FunEvent/FunEvent-memberPage/js/memberPage.js
@@ -3,19 +3,21 @@ console.log('Hi ! you have linked memberPage.js');
 
 // 會員基本資料修改
 // 按鈕跳入表單
-let infoEditBtnEle = document.querySelector("#member-info-edit-btn"); // 修改按鈕
-let infoSaveBtnEle = document.querySelector("#member-info-save-btn"); // 儲存(修改)按鈕
-let infoContentEle = document.querySelector("#member-info-content");  // 基本資料頁面div
-let infoEditEle = document.querySelector("#member-info-edit");        // 修改資料頁面div
-// 點下編輯按鈕
+let infoEditBtnEle = document.querySelector("#member-info-edit-btn");     // 修改按鈕
+let infoCancelBtnEle = document.querySelector("#member-info-cancel-btn"); // 取消按鈕
+let infoSaveBtnEle = document.querySelector("#member-info-save-btn");     // 儲存(修改)按鈕
+let infoContentEle = document.querySelector("#member-info-content");      // 基本資料頁面div
+let infoEditEle = document.querySelector("#member-info-edit");            // 修改資料頁面div
+// 按下編輯按鈕
 infoEditBtnEle.onclick = () => {
     infoContentEle.classList.remove("active");
     infoEditEle.classList.add("active");
 };
-// 按下儲存(修改)按鈕
-infoSaveBtnEle.onclick = () => {
+// 按下取消按鈕
+infoCancelBtnEle.onclick = () => {
     infoEditEle.classList.remove("active");
     infoContentEle.classList.add("active");
 };
+// 按下儲存按鈕
 
 // 會員管理的tab和tab content
