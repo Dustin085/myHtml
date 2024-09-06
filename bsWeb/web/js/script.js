7b7ed5d50216e85a7b3f12d98ab137cca80876ff
Dustin085 <dustin830927@gmail.com>
2024-09-06 17:13:44 +0800

    0906

--- bsWeb/web/js/script.js
+++ bsWeb/web/js/script.js
@@ -1,4 +1,18 @@
 $(function () {
     // 啟用jquery ui的tabs功能
     $("#tabs").tabs();
+
+    // 滑鼠變化影像
+    $('.hover-image').hover(
+        function () {
+            // 滑鼠移入時更換圖片
+            let hoverSrc = $(this).data('hover');
+            $(this).attr('data-default', $(this).attr('src')); // 儲存原始圖片
+            $(this).attr('src', hoverSrc);
+        },
+        function () {
+            // 滑鼠移出時還原圖片
+            let defaultSrc = $(this).attr('data-default');
+            $(this).attr('src', defaultSrc);
+        });
 });
